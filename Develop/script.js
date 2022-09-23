643f3d8c6afad0c9acb1b80ccab1e4b7217a74e5
justinberg <justinmberg97@gmail.com>
2022-09-22 22:30:10 -0500

    final push

--- Develop/script.js
+++ Develop/script.js
@@ -1,33 +1,45 @@
-const timeBlocks = document.querySelectorAll("textarea")
+const timeBlocks = document.querySelectorAll("textarea");
 
-
-// Create Time of Day Display 
+// Create Time of Day Display
 var time = moment().format("MMM Do, YYYY, hh:mm:ss");
 $("#currentDay").text(time);
-
-
+// Create Time Block color formatting
 function assessTimeBlocks() {
-    let startTime = 9
+  let startTime = 9;
 
-const currentHour = moment().hour();
-console.log(currentHour)
+  const currentHour = moment().hour();
+  console.log(currentHour);
 
-timeBlocks.forEach(timeBlock => {
-    console.log(timeBlock, currentHour, startTime)
-    if (currentHour === startTime) {timeBlock.classList.add('present')}
-    if (currentHour < startTime) {timeBlock.classList.add('future')}
-    if (currentHour > startTime) {timeBlock.classList.add('past')}
-    startTime ++
-}) 
+  timeBlocks.forEach((timeBlock) => {
+    console.log(timeBlock, currentHour, startTime);
+    if (currentHour === startTime) {
+      timeBlock.classList.add("present");
+    }
+    if (currentHour < startTime) {
+      timeBlock.classList.add("future");
+    }
+    if (currentHour > startTime) {
+      timeBlock.classList.add("past");
+    }
+    startTime++;
+  });
 }
+// Create local storage functionality 
 function saveNote() {
-var siblings = $(this).siblings()
-var text = siblings[1].value
-var timeBlockId = siblings[1].getAttribute("id")
-console.log(timeBlockId,text)
-localStorage.setItem(timeBlockId, text);
+  var siblings = $(this).siblings();
+  console.log(siblings)
+  var text = siblings[1].value;
+  var timeBlockId = siblings[1].getAttribute("id");
+  console.log(timeBlockId, text);
+  localStorage.setItem(timeBlockId, text);
 }
+  for (let i = 1; i <= 16; i++){
+    // let key = localStorage.timeBlockId(i);
+    let value = localStorage.getItem(i);
+    $("#" + i).val(value);
+  }
+  
+
 $(".saveBtn").click(saveNote);
 
 assessTimeBlocks()
-
